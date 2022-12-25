35fff0ca9ea6791a8a04122f77d546ad62558428
hitesh-sysorex <hiteshsysorex@gmail.com>
2022-12-25 10:57:40 +0530

    singlechild

--- lib/Layout/callscreens/pickup/pickup_screen.dart
+++ lib/Layout/callscreens/pickup/pickup_screen.dart
@@ -45,64 +45,66 @@ class _PickupScreenState extends State<PickupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        padding: const EdgeInsets.symmetric(vertical: 100),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              "Incoming...",
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-            const SizedBox(height: 30),
-            Image.network(
-              widget.call.callerPic!,
-              // isRound: true,
-              // radius: 180,
-            ),
-            const SizedBox(height: 15),
-            Text(
-              widget.call.callerName!,
-              style: const TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(height: 75),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(Icons.call_end),
-                  color: Colors.redAccent,
-                  onPressed: () async {
-                    isCallMissed = false;
-                    // addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
-                    await callMethods.endCall(call: widget.call);
-                  },
+      body: SingleChildScrollView(
+        child: Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(vertical: 100),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Incoming...",
+                style: TextStyle(
+                  fontSize: 30,
                 ),
-                const SizedBox(width: 25),
-                IconButton(
-                    icon: const Icon(Icons.call),
-                    color: Colors.green,
+              ),
+              const SizedBox(height: 30),
+              Image.network(
+                widget.call.callerPic!,
+                // isRound: true,
+                // radius: 180,
+              ),
+              const SizedBox(height: 15),
+              Text(
+                widget.call.callerName!,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(height: 75),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.call_end),
+                    color: Colors.redAccent,
                     onPressed: () async {
                       isCallMissed = false;
                       // addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
-                      // await Permissions.cameraAndMicrophonePermissionsGranted()
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CallScreen(call: widget.call),
-                        ),
-                      );
-                      // : {};
-                    }),
-              ],
-            ),
-          ],
+                      await callMethods.endCall(call: widget.call);
+                    },
+                  ),
+                  const SizedBox(width: 25),
+                  IconButton(
+                      icon: const Icon(Icons.call),
+                      color: Colors.green,
+                      onPressed: () async {
+                        isCallMissed = false;
+                        // addToLocalStorage(callStatus: CALL_STATUS_RECEIVED);
+                        // await Permissions.cameraAndMicrophonePermissionsGranted()
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CallScreen(call: widget.call),
+                          ),
+                        );
+                        // : {};
+                      }),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
